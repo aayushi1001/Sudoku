eb28d1c8a77fd2ba60ef91038327d49e7102f539
aayushi1001 <aayushianand08@gmail.com>
2021-04-18 07:02:14 +0530

    added check function and timer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import generator from "sudoku";
 import './App.css';
 import SudokuBoard from "./components/SudokuBoard"
+import produce from "immer";
 
-window.generator = generator;
 
 /*
 creating the matrix for sudoku
@@ -13,16 +13,26 @@ Others would be null for the user to enter
 
 function generateSudoku(){
   const raw = generator.makepuzzle();
-  const result = { rows: [] };
+  const rawSolution = generator.solvepuzzle(raw);
+
+  const formatted = raw.map(e => (e === null ? null : e + 1 ));
+  const formattedSolution = rawSolution.map(e => e + 1);
+
+  const result = { 
+    rows: [], 
+    solution:formattedSolution,
+    startTime: new Date(),
+    solvedTime : null
+  };
 
   for(let i=0;i<9;++i){
     const row = {cols: [] , index:i};
     for(let j=0;j<9;++j){
-      const value = raw[i*9+j]     /* you would actually get the value of a particular cell that is assigned to you by this function make.puzzel() */
+      const value = raw[i*9+j]    /* you would actually get the value of a particular cell that is assigned to you by this function make.puzzel() */
       const col = {
         row:i,
         col:j,
-        value:value,
+        value:value === null? null : value+1,
         readonly: value !== null
       };
       row.cols.push(col);
@@ -32,21 +42,62 @@ function generateSudoku(){
 return result;
 }
 
+
+function checkSolution(sudoku){
+  const candidate = sudoku.rows.map((row) => row.cols.map((col) => col.value)).flat()
+
+  for(let i=0;i<candidate.length;++i){
+    if(candidate[i] === null || candidate[i] !== sudoku.solution[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
 class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = {
-      sudoku: generateSudoku()
-    }
+    this.state = produce({},() =>({             /* using immer makes this state immutable here */
+        sudoku: generateSudoku()
+    }));
   }
+
+  handleChange = e => {
+    this.setState(
+      produce(state => {
+      state.sudoku.rows[e.row].cols[e.col].value = e.value; 
+      if(!state.sudoku.solvedTime){
+        const solved = checkSolution(state.sudoku) 
+        if(solved){
+          state.sudoku.solvedTime = new Date();  
+        }
+      }
+    })
+    );
+  }
+
+ 
+  solveSudoku = e => {
+    this.setState(
+      produce(state => {
+        state.sudoku.rows.forEach(row => 
+          row.cols.forEach(col => {
+            col.value = state.sudoku.solution[col.row * 9 + col.col];
+        })    
+        );
+      })
+    );
+  };
+
   render() {
     return(
     <div className="App">
       <header className="App-header">
-        <h1>Sudoku</h1>
+        <h3>Here's Your Sudoku Challenge!!</h3>
       </header>
-      <SudokuBoard sudoku={this.state.sudoku}/>
+      <SudokuBoard sudoku={this.state.sudoku} onChange={this.handleChange} />
+      <button onClick = {this.solveSudoku}>Can't Solve? Get the ans.</button>
     </div>
     )
   }
